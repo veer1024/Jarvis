a75f4502f2f84b633cbdd5593af144c52e5fdb8a
root <root@localhost.localdomain>
2022-06-14 16:06:59 +0530

    last commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y
-apt-get install docker.io -y
-docker pull hackpeas/jarvis-the-hunter:1.0
-echo -e "\e[1;31m Setup is done, run start.sh \e[0m"
+apt-get update 1>/dev/null 2>error.txt && apt-get upgrade -y 1>/dev/null 2>error.txt 
+apt-get install python3 1>/dev/null 2>error.txt
+apt-get install pip3 1>/dev/null 2>error.txt
+pip3 install -r requirements.txt 1>/dev/null 2>error.txt
+python3 banner.py
+#echo -e "\e[1;31m Installation will begun shortly \e[0m" 
+apt-get install docker.io -y 1>/dev/null 2>error.txt
+docker pull hackpeas/jarvis-the-hunter:1.0 1>/dev/null 2>error.txt
+cat error.txt 2>/dev/null
+echo -e "\e[1;31m Setup is done, run start.sh \e[0m" 
